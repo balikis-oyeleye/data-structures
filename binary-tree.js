e874e73f73cfc80f4413ba2232455ddeb47c14e0
balikis.dev <96819668+balikis-oyeleye@users.noreply.github.com>
2025-03-24 05:55:17 +0100

    Update binary-tree.js

--- binary-tree.js
+++ binary-tree.js
@@ -110,4 +110,35 @@ class BinarySearchTree{
       return this.min(root.right)
     }
   }
+  
+  delete(value){
+    this.root = this.deleteNode(this.root, value)
+  }
+  
+deleteNode(root, value) {
+  if (!root) return root;
+  
+  if (value < root.value) {
+    root.left = this.deleteNode(root.left, value);
+  } else if (value > root.value) {
+    root.right = this.deleteNode(root.right, value);
+  } else {
+    if (!root.left && !root.right) {
+      return null;
+    }
+    if (!root.left) {
+      return root.right;
+    }
+    if (!root.right) {
+      return root.left;
+    }
+    
+    root.value = this.min(root.right);
+    
+    root.right = this.deleteNode(root.right, root.value);
+  }
+
+  return root;
+}
+
 }
